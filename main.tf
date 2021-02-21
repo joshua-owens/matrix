2aa6d80b190a7ffd81c11ae3ff9b16f558384270
Josh Owens <me@jowens.dev>
2021-02-21 11:30:01 -0600

    Switching aws instance over to us east 1

--- main.tf
+++ main.tf
@@ -25,13 +25,13 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-03d315ad33b9d49c4"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
